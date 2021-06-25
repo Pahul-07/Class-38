aad84942c9f47d81d2f0b5f7e2b9b0cc93ba3205
Pahul-07 <pahuldeep2021@gmail.com>
2021-06-25 16:48:18 +0530

    Class 38

--- sketch.js
+++ sketch.js
@@ -4,6 +4,10 @@ function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    database = firebase.database();
+
+    database.ref('ball/position').on("value",readStock,showError);
 }
 
 function draw(){
@@ -26,4 +30,19 @@ function draw(){
 function changePosition(x,y){
     ball.x = ball.x + x;
     ball.y = ball.y + y;
+
+    database.ref('ball/position').set({
+        x: ball.x,
+        y: ball.y
+    })
+}
+
+function readStock(data){
+    var position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;   
+}
+
+function showError(){
+    console.log("There is an error from reading the values from database");
 }
